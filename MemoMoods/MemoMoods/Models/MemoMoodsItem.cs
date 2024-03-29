b9fedd1aa3c7b34e3e259621ceae10e4e9200199
ellaTerselic <terseler@mail.uc.edu>
2024-03-29 11:49:45 -0400

    date functionality added

--- MemoMoods/MemoMoods/Models/MemoMoodsItem.cs
+++ MemoMoods/MemoMoods/Models/MemoMoodsItem.cs
@@ -9,11 +9,22 @@ namespace MemoMoods.Models
     {
         [PrimaryKey, AutoIncrement]
         public int ID { get; set; }
+        public DateTime Date { get; set; }
         public int CowMood { get; set; }
         public string Question1 { get; set; }
         public string Question2 { get; set; }
         public string Question3 { get; set; }
         public string JournalEntry {  get; set; }
         public bool GoalsMet { get; set; }
+
+        public MemoMoodsItem()
+        {
+            Date = DateTime.Now.Date;
+            
+        }
+        public string FormattedDate
+        {
+            get { return Date.ToString("MM-dd-yy"); } 
+        }
     }
 }
